483b04e66a72b917377944b6d2028ebb2af18241
enjxyer <hfgtylop@gmail.com>
2026-01-09 16:07:51 +0500

    feat: add new templates and connect them in urls

--- core/views.py
+++ core/views.py
@@ -1,1 +1,9 @@
 from django.shortcuts import render
+from .models import Game
+
+def gameList(request):
+    games = Game.objects.all().prefetch_related("prices", "prices__store")
+    for g in games:
+        g.latest_price = g.get_current_price()
+        g.lowest_price = g.get_lowest_price()
+    return render(request, "gameList.html", {"games": games})
